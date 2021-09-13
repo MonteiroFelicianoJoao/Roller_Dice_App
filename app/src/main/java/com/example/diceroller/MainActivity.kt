16f08b486d4ee8cbdc03bfb1ed0c2f65eaf2b939
MonteiroFelicianoJoao <57365237+MonteiroFelicianoJoao@users.noreply.github.com>
2021-09-13 00:56:51 -0300

    Adding a second dice.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import java.lang.Math.random
+import com.example.diceroller.R.string.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,12 +20,16 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        (getString(dice1_text) + " " + diceRoll.toString()).also { resultTextView.text = it }
+
+        val dice1 = Dice(6)
+        val diceRoll1 = dice1.roll()
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        (getString(dice2_text) + " " + diceRoll1.toString()).also { resultTextView2.text = it }
     }
 }
 
 class Dice (private val numSides: Int) {
-
     fun roll(): Int {
         return (1..numSides).random()
     }
