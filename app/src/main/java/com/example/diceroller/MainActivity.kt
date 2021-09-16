6d8555e0d2797ae79c1941d114178bd5eb188406
MonteiroFelicianoJoao <57365237+MonteiroFelicianoJoao@users.noreply.github.com>
2021-09-15 21:35:24 -0300

    App with visual dice.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import com.example.diceroller.R.string.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,14 +12,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { diceRoll() }
     }
 
-    private fun rollDice() {
+    private fun diceRoll() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         val diceImage: ImageView = findViewById(R.id.diceImageView)
-        diceImage.setImageResource(R.drawable.dice_2)
+
+        when(diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
